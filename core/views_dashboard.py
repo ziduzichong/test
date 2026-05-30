dadf1e5e37ca57f987012d30a782104fbdda63da
ziduzichong <2086496319@qq.com>
2026-05-31 04:07:23 +0800

    fix: 补充HttpResponse导入 - toggle/delete端点500修复

--- core/views_dashboard.py
+++ core/views_dashboard.py
@@ -6,7 +6,7 @@ import os
 import hashlib
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseForbidden, JsonResponse, FileResponse
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse, FileResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
